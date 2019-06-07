d600bbbc4dd75ab7f50a5c0d714f2cd5f4e43d32
Øystein Gregers Lindbjør <ogl@dips.no>
2019-06-07 11:32:45 +0200

    Add missing codebehind

---
+++ CefSharp.MinimalExample.Wpf/BrowserWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class BrowserWindow : UserControl
+    {
+        public BrowserWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
